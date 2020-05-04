e578956a009e8fbad9ba6bb5786a4ba7f67046d1
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-05-04 15:42:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(int)
-  if int > 1 && 1 == (2...100).to_a.select{|num| int % num == 0}.length
+  if int > 1 && 1 == (2...50000).to_a.select{|num| int % num == 0}.length
     true
   else
     false
