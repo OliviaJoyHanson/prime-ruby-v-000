332e48eb7110dc07707755fe1cd07b03854198ae
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-05-04 15:47:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,8 @@
 # Add  code here!
 def prime?(int)
-  if int > 1 && !(2...(int-1)).to_a.any?{|num| int % num == 0}
+  if int == 2
+    true
+  if int > 1 && (2...(int-1)).to_a.any?{|num| int % num == 0}
     true
   else
     false
